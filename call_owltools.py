8fdab3ad3feda48ad862793efdcb4b1382c6ef7d
Heiko Dietze <hdietze@lbl.gov>
2015-11-18 10:33:30 -0800

    add classpath self test

--- call_owltools.py
+++ call_owltools.py
@@ -1,1 +1,8 @@
-print owltools.graph.OWLGraphWrapper.getClass().getName()
+#-- self test to see what is on the classpath
+from java.lang import ClassLoader
+cl = ClassLoader.getSystemClassLoader()
+paths = map(lambda url: url.getFile(), cl.getURLs())
+print paths
+#----- try to access an owltools class
+from owltools.graph import OWLGraphWrapper
+print OWLGraphWrapper.getClass().getName()
